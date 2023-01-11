7482e8bee69b6b4354973eb549e712ab10669cdb
Tresor <89020710+Mbonyimfura@users.noreply.github.com>
2023-01-11 18:57:09 +0200

    Update db.js

--- model/db.js
+++ model/db.js
@@ -5,7 +5,7 @@ main().catch(err => console.log(err));
 async function main() {
  // await mongoose.connect('mongodb://127.0.0.1:27017/EmployeDB');
   
-   await mongoose.connect('mongodb+srv://taskapp:rwamagana@cluster0.qiexf75.mongodb.net/EmployeDB?',{useNewUrlParser: true}); 
+   await mongoose.connect('url',{useNewUrlParser: true}); 
    mongoose.set("strictQuery", true);
 }
 require('./employee.model')
